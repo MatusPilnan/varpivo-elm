c46341c663be2a0c8648bc04f2087a38a8995aeb
MatusPilnan <m.pilnan@gmail.com>
2021-01-31 18:31:01 +0100

    add PWA support (attempt 5)

---
+++ assets/config.js
@@ -0,0 +1,3 @@
+const config = {
+  BASE_PATH: '/varpivo-elm'
+}
--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,14 @@
+importScripts('assets/config.js')
+
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open('varpivo').then(function (cache) {
       return cache.addAll([
-        '/',
-        '/manifest.json',
-        '/elm.js',
-        '/assets/images/icon.png',
-        '/assets/css/app.css',
+        config.BASE_PATH + '/',
+        config.BASE_PATH + '/manifest.json',
+        config.BASE_PATH + '/elm.js',
+        config.BASE_PATH + '/assets/images/icon.png',
+        config.BASE_PATH + '/assets/css/app.css',
       ]);
     })
   );
